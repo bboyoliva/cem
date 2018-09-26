0d8c612d4ffeaea678e70db161eed2db21069607
Jesus Oliva <jesus241294@gmail.com>
2018-09-26 17:29:44 +0200

    Añadimos dependencias y creamos componentres

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { NavegacionComponent } from './componentes/navegacion/navegacion.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { PrivadoComponent } from './componentes/privado/privado.component';
+import { NotFoundComponent } from './componentes/not-found/not-found.component';
+import { InicioPageComponent } from './componentes/inicio-page/inicio-page.component';
+import { LoginPageComponent } from './componentes/login-page/login-page.component';
+import { PrivadoPageComponent } from './componentes/privado-page/privado-page.component';
+import { NotFoundPageComponent } from './componentes/not-found-page/not-found-page.component';
+import { RegistroPageComponent } from './componentes/registro-page/registro-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    NavegacionComponent,
+    LoginComponent,
+    PrivadoComponent,
+    NotFoundComponent,
+    InicioPageComponent,
+    LoginPageComponent,
+    PrivadoPageComponent,
+    NotFoundPageComponent,
+    RegistroPageComponent
   ],
   imports: [
     BrowserModule,
